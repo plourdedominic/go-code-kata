97ce055606cd8b631170c5bf7b629c197c716858
CodeSandbox <support@codesandbox.io>
2023-06-21 18:15:40 +0000

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module main
+
+require (
+    github.com/stretchr/testify v1.8.1
+)
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Go in CodeSandbox!")
+}
+
+func returnTrue() bool {
+	return true
+}
